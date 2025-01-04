89f326cf95b993c692b38d93c5b7ec75c5a4f983
Divyakshi Tare <divyakshitare09@gmail.com>
2025-01-04 14:31:02 +0530

    Update run.py

--- Todo-List/run.py
+++ Todo-List/run.py
@@ -6,13 +6,3 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-# from flask import Flask, render_template
-# app= Flask (__name__)
-
-# @app.route('/')
-# def hello_world():
-#     return render_template('index.html')
-#     # return 'Hello, World!'
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
